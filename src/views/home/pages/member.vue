ac562d74d4a3664ca5a5151c6458f8354f36fae0
qinxianzong <qinxianzong0525@gmail.com>
2018-04-04 10:15:12 +0800

    科普页面结束 footer菜单界面美化完毕 我的(个人中心)待完善

--- src/views/home/pages/member.vue
+++ src/views/home/pages/member.vue
@@ -3,24 +3,28 @@
     <div class="head"></div>
     <div class="list-box">
       <ul>
-        <li v-for="item in links"><a href=""></a></li>
+        <li v-for="item in memberMenu" :key="item.id"><a href="">aa</a></li>
       </ul>
     </div>
-    <a href="javascript:void(0)" class="loginout" @click="quit()">退出登录</a>
+    <!--<a href="javascript:void(0)" class="loginout" @click="quit()">退出登录</a>-->
   </div>
 </template>
 
 <script>
     export default {
-        name: "member",
-      links: [
-        {title:"健康档案",url:""},
-        {title:"我的收藏",url:""},
-        {title:"密码修改",url:""},
-        {title:"意见反馈",url:""},
-        {title:"检查更新",url:""},
-        {title:"关于我们",url:""}
-      ]
+      name: "member",
+      data: {
+        return: {
+          memberMenu: [
+            {title:"健康档案",url:""},
+            {title:"我的收藏",url:""},
+            {title:"密码修改",url:""},
+            {title:"意见反馈",url:""},
+            {title:"检查更新",url:""},
+            {title:"关于我们",url:""}
+          ]
+        }
+      }
     }
 </script>
 
