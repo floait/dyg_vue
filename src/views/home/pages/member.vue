c9c49a237d85e7b2b5a9d2de74b72893cef3e467
qinxianzong <qinxianzong0525@gmail.com>
2018-04-04 16:28:02 +0800

    科普页面结束 footer菜单界面美化完毕 我的(个人中心)待完善

--- src/views/home/pages/member.vue
+++ src/views/home/pages/member.vue
@@ -38,43 +38,42 @@
         }
       },
       methods: {
-        WebSocketTest()
-        {
-          if ("WebSocket" in window)
-          {
-            alert("您的浏览器支持 WebSocket!");
-
-            // 打开一个 web socket
-            let ws = new WebSocket("ws://localhost:8099");
-
-            ws.onopen = function()
-            {
-              // Web Socket 已连接上，使用 send() 方法发送数据
-              ws.send("发送数据");
-              alert("数据发送中...");
-            };
-
-            ws.onmessage = function (evt)
-            {
-              let received_msg = evt.data;
-              alert("数据已接收...");
-              console.log(received_msg);
-            };
-
-            ws.onclose = function(e)
-            {
-              // 关闭 websocket
-              alert("连接已关闭...");
-              console.log(e);
-            };
-          }
-
-          else
-          {
-            // 浏览器不支持 WebSocket
-            alert("您的浏览器不支持 WebSocket!");
-          }
-        }
+        // WebSocketTest() {
+        //   if ("WebSocket" in window)
+        //   {
+        //     alert("您的浏览器支持 WebSocket!");
+        //
+        //     // 打开一个 web socket
+        //     let ws = new WebSocket("ws://localhost:8099");
+        //
+        //     ws.onopen = function()
+        //     {
+        //       // Web Socket 已连接上，使用 send() 方法发送数据
+        //       ws.send("发送数据");
+        //       alert("数据发送中...");
+        //     };
+        //
+        //     ws.onmessage = function (evt)
+        //     {
+        //       let received_msg = evt.data;
+        //       alert("数据已接收...");
+        //       console.log(received_msg);
+        //     };
+        //
+        //     ws.onclose = function(e)
+        //     {
+        //       // 关闭 websocket
+        //       alert("连接已关闭...");
+        //       console.log(e);
+        //     };
+        //   }
+        //
+        //   else
+        //   {
+        //     // 浏览器不支持 WebSocket
+        //     alert("您的浏览器不支持 WebSocket!");
+        //   }
+        // }
       }
 
     }
