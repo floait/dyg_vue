e67d22f458f1482c9e9bc3222f9ce490028c7acf
qinxianzong <qinxianzong0525@gmail.com>
2018-09-03 10:00:30 +0800

    Update proxyConfig.js

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -1,32 +1,11 @@
 module.exports = {
   proxy: {
-    '/hecdoctor': {  //baseUrl
+    '/apis': {  //baseUrl
       target: 'http://********.com/', // 接口域名
-      changeOrigin: true, //是否跨域
-      pathRewrite: {
-        '^/hecdoctor': ''  //需要rewrite的,
-      }
-    },
-    '/apis': {  //api接口地址
-      target: 'http://*********.com/api/user', // 接口域名
       changeOrigin: true, //是否跨域
       pathRewrite: {
         '^/apis': ''  //需要rewrite的,
       }
     },
-    '/article': {  //文章
-      target: 'http://*********.com/api/user/article', // 接口域名
-      changeOrigin: true, //是否跨域
-      pathRewrite: {
-        '^/article': ''  //需要rewrite的,
-      }
-    },
-    '/article': {  //文章
-      target: 'http://*********.com/api/user/article', // 接口域名
-      changeOrigin: true, //是否跨域
-      pathRewrite: {
-        '^/article': ''  //需要rewrite的,
-      }
-    }
   }
 }
